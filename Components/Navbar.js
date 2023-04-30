7bd35466a27d069fb9f6af329528c906fd512055
Thiago Martins <thifelipe23@gmail.com>
2023-04-29 23:45:35 -0300

    feat: adjustments for bro

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,25 +1,40 @@
-import React, { useState, useEffect } from "react";
-import styles from "../styles/Navbar.module.css";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { RiMenuAddFill } from "react-icons/ri";
-import Link from "next/link";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { RiMenuAddFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+function NavItem({ title, items }) {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <li
+      className={styles.submenu}
+      onMouseEnter={() => setOpenMenu(true)}
+      onMouseLeave={() => setOpenMenu(false)}
+    >
+      <Link href=''>
+        {title}
+        <RiMenuAddFill size={15} style={{ paddingLeft: '5px' }} />
+      </Link>
+
+      <ul className={`${openMenu ? styles['anim-up'] : ''}`}>
+        {items.map((item, index) => (
+          <li key={index}>
+            <Link href={item.href}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+}
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
-  const [showSecuritySubMenu, setShowSecuritySubMenu] = useState(false);
-  const [showStreamingSubMenu, setShowStreamingSubMenu] = useState(false);
   const [showBrowsersSubMenu, setShowBrowsersSubMenu] = useState(false);
   const [showWindowsSubMenu, setShowWindowsSubMenu] = useState(false);
-
-  const handleToggleSecuritySubMenu = () => {
-    setShowSecuritySubMenu(!showSecuritySubMenu);
-  };
-
-  const handleToggleStreamingSubMenu = () => {
-    setShowStreamingSubMenu(!showStreamingSubMenu);
-  };
 
   const handleToggleBrowsersSubMenu = () => {
     setShowBrowsersSubMenu(!showBrowsersSubMenu);
@@ -40,58 +55,44 @@ function Navbar() {
         <div className={click ? styles.navmenu : styles.navmenuactive}>
           <ul className={styles.navmenu}>
             <li onClick={handleMenuClick} className={styles.submenu}>
-              <Link href="/">Home</Link>
+              <Link href='/'>Home</Link>
             </li>
-            <li
-              className={styles.submenu}
-              onMouseEnter={handleToggleSecuritySubMenu}
-              onMouseLeave={handleToggleSecuritySubMenu}
-            >
-              <Link href="">
-                Speak Tools
-                <RiMenuAddFill size={15} style={{ paddingLeft: "5px" }} />
-              </Link>
-              {showSecuritySubMenu && (
-                <ul>
-                  <li>
-                    <Link href="/communication/discord">Discord Updated</Link>
-                  </li>
-                  <li>
-                    <Link href="/communication/zoom">Zoom Video</Link>
-                  </li>
-                </ul>
-              )}
-            </li>
-            <li
-              className={styles.submenu}
-              onMouseEnter={handleToggleStreamingSubMenu}
-              onMouseLeave={handleToggleStreamingSubMenu}
-            >
-              <Link href="">
-                Streaming
-                <RiMenuAddFill size={15} style={{ paddingLeft: "5px" }} />
-              </Link>
-              {showStreamingSubMenu && (
-                <ul>
-                  <li>
-                    <Link href="/streamming/obs-studio">OpenBroadCaster</Link>
-                  </li>
-                </ul>
-              )}
-            </li>
+            <NavItem
+              title='Speak Tools'
+              items={[
+                {
+                  href: '/communication/discord',
+                  title: 'Discord Updated'
+                },
+                {
+                  href: '/communication/zoom',
+                  title: 'Zoom Video'
+                }
+              ]}
+            />
+            <NavItem
+              title='Streaming'
+              items={[
+                {
+                  href: '/streamming/obs-studio',
+                  title: 'OpenBroadCaster'
+                }
+              ]}
+            />
+
             <li
               className={styles.submenu}
               onMouseEnter={handleToggleBrowsersSubMenu}
               onMouseLeave={handleToggleBrowsersSubMenu}
             >
-              <Link href="">
+              <Link href=''>
                 Browsers
-                <RiMenuAddFill size={15} style={{ paddingLeft: "5px" }} />
+                <RiMenuAddFill size={15} style={{ paddingLeft: '5px' }} />
               </Link>
               {showBrowsersSubMenu && (
                 <ul>
                   <li>
-                    <Link href="/browsers/brave">Brave</Link>
+                    <Link href='/browsers/brave'>Brave</Link>
                   </li>
                 </ul>
               )}
@@ -101,19 +102,19 @@ function Navbar() {
               onMouseEnter={handleToggleWindowsSubMenu}
               onMouseLeave={handleToggleWindowsSubMenu}
             >
-              <Link href="">
+              <Link href=''>
                 Windows
-                <RiMenuAddFill size={15} style={{ paddingLeft: "5px" }} />
+                <RiMenuAddFill size={15} style={{ paddingLeft: '5px' }} />
               </Link>
               {showWindowsSubMenu && (
                 <ul>
                   <li>
-                    <Link href="/windows/visual-studio-code">
+                    <Link href='/windows/visual-studio-code'>
                       Visual Studio Code
                     </Link>
                   </li>
                   <li>
-                    <Link href="/windows/netbeans-8.2">
+                    <Link href='/windows/netbeans-8.2'>
                       NetBeans IDE 8.2 Download
                     </Link>
                   </li>
@@ -121,15 +122,15 @@ function Navbar() {
               )}
             </li>
             <li>
-              <Link href="/contactus">Contact Us</Link>
+              <Link href='/contactus'>Contact Us</Link>
             </li>
           </ul>
         </div>
         <div className={styles.hamburguer} onClick={handleClick}>
           {click ? (
-            <FaTimes size={20} style={{ color: "#fff" }} />
+            <FaTimes size={20} style={{ color: '#fff' }} />
           ) : (
-            <FaBars size={20} style={{ color: "#fff" }} />
+            <FaBars size={20} style={{ color: '#fff' }} />
           )}
         </div>
       </div>
